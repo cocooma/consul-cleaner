9f13ea62b5d126b4666c4985ed7ceff1dee9246a
Oszkar Nagy <oszkarnagy@notonthehighstreet.com>
2016-05-20 09:14:29 +0100

    few more fixes

--- main.go
+++ main.go
@@ -1,22 +1,23 @@
 package main
 
 import (
+	"bufio"
+	"consul-cleaner/awsdiscovery"
 	"fmt"
 	"os"
+	"strings"
 	"sync"
-
-	"consul-cleaner/awsdiscovery"
 
 	flag "github.com/docker/docker/pkg/mflag"
 	"github.com/hashicorp/consul/api"
 )
 
 var (
-	str, url, port, srvState, awsregion, tag, tagvalue, hostdiscovery                                                         string
-	showTargetHosts, showMmemberStatus, showChecks, showAllSrv, deregisterSrv, listSrvInState, showNodeStatus, forceLeaveNode bool
-	nsc                                                                                                                       int
-	hosts                                                                                                                     []string
-	wg                                                                                                                        sync.WaitGroup
+	str, url, port, srvState, awsregion, tag, tagvalue, hostdiscovery, line                                                           string
+	listTargetHosts, showMmemberStatus, listChecks, listAllSrv, deregisterSrv, listSrvInState, listNodeStatus, forceLeaveNode, dryRun bool
+	nsc                                                                                                                               int
+	hosts                                                                                                                             []string
+	wg                                                                                                                                sync.WaitGroup
 )
 
 func connection(uurl, pport string) *api.Client {
@@ -43,32 +44,33 @@ func awshosts(awsregion, tag, tagvalue string) []string {
 	return ips
 }
 
-func showtargethost(ips []string) {
+func readHostsFromStdin() []string {
+	var ips []string
+	scanner := bufio.NewScanner(os.Stdin)
+	if scanner.Scan() {
+		line = scanner.Text()
+		lines := strings.Split(line, " ")
+		for _, word := range lines {
+			ips = append(ips, word)
+		}
+	}
+	return ips
+}
+
+func listTargetHost(ips []string) {
 	for _, server := range ips {
 		fmt.Println(server)
 	}
 }
 
-func showNodeStaus(consulClient *api.Client) {
+func listNodeStaus(consulClient *api.Client) {
 	members, _ := consulClient.Agent().Members(false)
 	for _, server := range members {
 		fmt.Printf("%s %v \n", server.Name, server.Status)
 	}
 }
 
-func forceLeaveBadNode(consulClient *api.Client, nodeStatusCode int) {
-	members, _ := consulClient.Agent().Members(false)
-	for _, server := range members {
-		if server.Status == nodeStatusCode {
-			err := consulClient.Agent().ForceLeave(server.Name)
-			if err != nil {
-				panic(err)
-			}
-		}
-	}
-}
-
-func listChecks(consulClient *api.Client) {
+func listCheck(consulClient *api.Client) {
 	checks, _ := consulClient.Agent().Checks()
 	for _, check := range checks {
 		fmt.Println(check.Node, check.Name, check.Status)
@@ -82,6 +84,13 @@ func listServices(consulClient *api.Client) {
 	}
 }
 
+func listServicesInState(consulConnection *api.Client, serviceCheckStatus string) {
+	service := serviceNameServiceID(consulConnection, serviceCheckStatus)
+	for serviceName, serviceID := range service {
+		fmt.Println(serviceName + " " + serviceID)
+	}
+}
+
 func serviceNameServiceID(connection *api.Client, serviceCheckStatus string) map[string]string {
 	services := map[string]string{}
 	serv, _, _ := connection.Health().State(serviceCheckStatus, nil)
@@ -91,45 +100,74 @@ func serviceNameServiceID(connection *api.Client, serviceCheckStatus string) map
 	return services
 }
 
-func listServicesInState(consulConnection *api.Client, serviceCheckStatus string) {
+func deregisterService(consulConnection *api.Client, serviceCheckStatus string) {
+	defer func() {
+		if e := recover(); e != nil {
+			fmt.Println("Whoops some error happend:", e)
+		}
+	}()
 	service := serviceNameServiceID(consulConnection, serviceCheckStatus)
-	for serviceName, serviceID := range service {
-		fmt.Println(serviceName + " " + serviceID)
+	if len(service) == 0 {
+		fmt.Println("There is no service in the given state: -" + serviceCheckStatus + "- available to deregister!!!")
+	} else {
+		for serviceName, serviceID := range service {
+			switch dryRun {
+			case false:
+				fmt.Println("SrvName: " + serviceName + "  SrvID: " + serviceID + " SrvStatus: " + serviceCheckStatus + " --- Has been deregistered!!!")
+				err := consulConnection.Agent().ServiceDeregister(serviceID)
+				if err != nil {
+					panic(err)
+				}
+			case true:
+				fmt.Println("Dryrun --- SrvName: " + serviceName + "  SrvID: " + serviceID + " SrvStatus: " + serviceCheckStatus + " --- Has been deregistered!!!")
+			}
+		}
 	}
 }
 
-func deregisterService(consulConnection *api.Client, serviceCheckStatus string) {
-	service := serviceNameServiceID(consulConnection, serviceCheckStatus)
-	for serviceName, serviceID := range service {
-		fmt.Println(serviceName + " " + serviceID + " has been deregistered!!!")
-		err := consulConnection.Agent().ServiceDeregister(serviceID)
-		if err != nil {
-			panic(err)
+func forceLeaveBadNode(consulClient *api.Client, nodeStatusCode int) {
+	defer func() {
+		if e := recover(); e != nil {
+			fmt.Println("Whoops some error happend:", e)
+		}
+	}()
+	members, _ := consulClient.Agent().Members(false)
+	for _, server := range members {
+		if server.Status == nodeStatusCode {
+			switch dryRun {
+			case false:
+				fmt.Println(server.Name + " --- Node force left the cluster!!!")
+				err := consulClient.Agent().ForceLeave(server.Name)
+				if err != nil {
+					panic(err)
+				}
+			case true:
+				fmt.Println("Dryrun --- " + server.Name + " --- Node force left the cluster!!!")
+			}
+		} else {
+			fmt.Println("there is no Node with status code: ", nodeStatusCode)
 		}
 	}
 }
 
 func main() {
-	flag.StringVar(&url, []string{"u", "-url"}, "localhost", "Consul members endpoint. Default: localhost")
+	flag.StringVar(&hostdiscovery, []string{"hd", "-hostDiscovery"}, "aws", "Host discovery. 'consul' or 'aws' or 'stdin'")
+	flag.StringVar(&url, []string{"u", "-url"}, "localhost", "Consul member endpoint. Default: localhost")
 	flag.StringVar(&port, []string{"p", "-port"}, "8500", "Consul members endpoint port. Default: 8500")
-	flag.IntVar(&nsc, []string{"nsc", "-nodeStatusCode"}, 4, "Node status code. Default: 4")
-	flag.BoolVar(&showTargetHosts, []string{"sth", "-showTargetHosts"}, false, "Show target hosts")
-	flag.BoolVar(&showNodeStatus, []string{"sns", "-showNodeStatus"}, false, "Show nodes status")
-	flag.BoolVar(&showChecks, []string{"schk", "-showChecks"}, false, "Show a list of checks")
-	flag.BoolVar(&showAllSrv, []string{"sasrv", "-showAllServices"}, false, "Show a list of services")
-	flag.StringVar(&srvState, []string{"ss", "-serviceState"}, "critical", "Deregister Service State. Default: critical")
-	flag.BoolVar(&deregisterSrv, []string{"drsrv", "-deregisterService"}, false, "Deregister service. Use it with -serviceState")
-	flag.BoolVar(&listSrvInState, []string{"lsrvis", "-listServiceInState"}, false, "Show a list of services")
 	flag.StringVar(&awsregion, []string{"ar", "-awsRegion"}, "eu-west-1", "AWS Region. Default: eu-west-1")
 	flag.StringVar(&tag, []string{"t", "-tag"}, "", "AWS tag")
 	flag.StringVar(&tagvalue, []string{"tv", "-tagValue"}, "", "AWS tag value")
-	flag.StringVar(&hostdiscovery, []string{"hd", "-hostDiscovery"}, "aws", "Host discovery. 'consul' or 'aws'")
-	flag.BoolVar(&forceLeaveNode, []string{"fl", "-forceLeaveNode"}, false, "Force leave consul node. Use it with -nodeStatusCode")
+	flag.IntVar(&nsc, []string{"nsc", "-nodeStatusCode"}, 4, "Consul node status code. Default: 4")
+	flag.BoolVar(&listTargetHosts, []string{"lth", "-listTargetHosts"}, false, "List target hosts")
+	flag.BoolVar(&listNodeStatus, []string{"lns", "-listNodeStatus"}, false, "List nodes status")
+	flag.BoolVar(&listChecks, []string{"lchk", "-listChecks"}, false, "List checks")
+	flag.BoolVar(&listSrvInState, []string{"lsrvis", "-listServiceInState"}, false, "List of services in specific state. Use it with --serviceState")
+	flag.BoolVar(&listAllSrv, []string{"lasrv", "-listAllServices"}, false, "List of all services")
+	flag.StringVar(&srvState, []string{"ss", "-serviceState"}, "critical", "State of the service you wish to deregister. Default: critical")
+	flag.BoolVar(&deregisterSrv, []string{"drsrv", "-deregisterService"}, false, "Deregister service. Use it with --serviceState")
+	flag.BoolVar(&forceLeaveNode, []string{"fl", "-forceLeaveNode"}, false, "Force leave consul node. Use it with --nodeStatusCode")
+	flag.BoolVar(&dryRun, []string{"d", "-dryrun"}, false, "Dryrun")
 	flag.Parse()
-
-	// var status = flag.String("status", "", "Consul Member Status for eviction")
-	// var Debug = flag.String("debug", "", "Debug information output")
-	// varDryrun = flag.String("cmd", "", "Do not remove the nodes, just preten")
 
 	consulClient := connection(url, port)
 
@@ -138,10 +176,12 @@ func main() {
 		hosts = consulmembers(consulClient)
 	case "aws":
 		hosts = awshosts(awsregion, tag, tagvalue)
+	case "stdin":
+		hosts = readHostsFromStdin()
 	}
 
-	if showTargetHosts == true {
-		showtargethost(hosts)
+	if listTargetHosts == true {
+		listTargetHost(hosts)
 		os.Exit(0)
 	}
 
@@ -165,37 +205,27 @@ func main() {
 		}
 	}
 
-	if showNodeStatus == true {
+	if listNodeStatus == true {
 		wg.Add(len(hosts))
 		for _, ip := range hosts {
 			go func(ip string) {
-				showNodeStaus(connection(ip, "8500"))
+				listNodeStaus(connection(ip, "8500"))
 				wg.Done()
 			}(ip)
 		}
 	}
 
-	if showChecks == true {
+	if listChecks == true {
 		wg.Add(len(hosts))
 		for _, ip := range hosts {
 			go func(ip string) {
-				listChecks(connection(ip, "8500"))
+				listCheck(connection(ip, "8500"))
 				wg.Done()
 			}(ip)
 		}
 	}
 
-	if showAllSrv == true {
-		wg.Add(len(hosts))
-		for _, ip := range hosts {
-			go func(ip string) {
-				listServices(connection(ip, "8500"))
-				wg.Done()
-			}(ip)
-		}
-	}
-
-	if showAllSrv == true {
+	if listAllSrv == true {
 		wg.Add(len(hosts))
 		for _, ip := range hosts {
 			go func(ip string) {
